daad31a09fdf300b9a8230f44e4f0ba7961ec13b
Terry Dennison Jr <107069502+Dog-Gone-Earl@users.noreply.github.com>
2023-03-19 08:48:21 -0600

    Add files via upload

---
+++ Vagrant_ubuntu_bionic_agent.rb
@@ -0,0 +1,21 @@
+Vagrant.configure("2") do |config|
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
+
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://vagrantcloud.com/search.
+  config.vm.box = "uwbbi/bionic-arm64"
+  config.vm.provider "vmware_desktop" do |v|
+    v.ssh_info_public = true
+    v.gui = true
+    v.linked_clone = false
+    v.vmx["ethernet0.virtualdev"] = "vmxnet3"
+    end 
+  config.vm.synced_folder "./shared", "/home/vagrant/shared", create: true
+  config.vm.provision "shell", privileged: true, inline: <<-SHELL
+  sudo apt-get update
+  yes y | sudo apt-get upgrade
+  DD_AGENT_MAJOR_VERSION=7 DD_API_KEY= DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
+  SHELL
+end
